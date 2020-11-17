4031e2ef53828c09903bef72d41fea94b3334cb3
priya-abc <priyapriya6598@gmail.com>
2020-11-17 18:56:20 +0530

    Added:Welcome

---
+++ AddressBook.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+public class AddressBook {
+private ArrayList<AddRec> addressRecords = new ArrayList<AddRec>();
+
+System.out.println("Welcome to AddressBook");
