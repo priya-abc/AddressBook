492909799acdb9fc75f8ba856c5821287c95b971
priya-abc <priyapriya6598@gmail.com>
2020-11-17 19:32:56 +0530

    Added: Added Records

--- AddressBook.java
+++ AddressBook.java
@@ -5,7 +5,7 @@ public class AddressBook {
 private ArrayList<AddRec> addressRecords = new ArrayList<AddRec>();
 
 System.out.println("Welcome to AddressBook");
-<<<<<<< HEAD
+<<<<<< HEAD
 
 Scanner in = new Scanner(System.in);
 	public void AddressBook()
@@ -15,7 +15,7 @@ Scanner in = new Scanner(System.in);
         String firstNames[]= new String[firstnames_count];
         System.out.println("Enter the First Names: ");
         for(int i=0;i<firstnames_count;i++) {
-=======
+
 Scanner in = new Scanner(System.in);
 	public void AddressBook()
 	{
@@ -24,21 +24,17 @@ Scanner in = new Scanner(System.in);
         String firstNames[]= new String[firstnames_count];
         System.out.println("Enter the First Names: ");
             for(int i=0;i<firstnames_count;i++) {
->>>>>>> UC1-MenuOptions
+
             firstNames[i]=in.next();
         }
         System.out.println("Enter the number of last names: ");
         int lastnames_count=in.nextInt();
-
-<<<<<<< HEAD
         if(firstnames_count!=lastnames_count) {
-=======
            if(firstnames_count!=lastnames_count) {
->>>>>>> UC1-MenuOptions
             System.out.println("Mismatch! Number of first names and last names should be same ");
             System.exit(0);
         }
-      
+
         String lastNames[]= new String[lastnames_count]; 
         System.out.println(" Enter the last names in order - matching the first names");
         for(int i=0;i<lastnames_count;i++) {
@@ -95,3 +91,54 @@ Scanner in = new Scanner(System.in);
 	
 	}while(menuOption.equals("6") == false);
 }
+private void addRecord() {
+    	 Scanner in = new Scanner(System.in);
+         LinkedHashMap<String,String> hm= new LinkedHashMap<>();
+         System.out.println("Enter the number of first names: ");
+         int firstnames_count= in.nextInt();
+         String firstNames[]= new String[firstnames_count];
+         System.out.println("Enter the First Names: ");
+         for(int i=0;i<firstnames_count;i++) {
+             firstNames[i]=in.next();
+         }
+         System.out.println("Enter the number of last names: ");
+         int lastnames_count=in.nextInt();
+
+         if(firstnames_count!=lastnames_count) {
+             System.out.println("Mismatch! Number of first names and last names should be same ");
+             System.exit(0);
+         }
+       
+         String lastNames[]= new String[lastnames_count]; 
+         System.out.println(" Enter the last names in order - matching the first names");
+         for(int i=0;i<lastnames_count;i++) {
+             lastNames[i]=in.next();
+         }
+
+         for(int i=0;i<firstnames_count;i++) {  
+             hm.put(firstNames[i],lastNames[i]);
+         }
+    
+         Map<String,String> result= sortMap(hm);
+         System.out.println("The Last Names in alphabetial order: ");
+         System.out.println();
+         for(Map.Entry<String, String> entry: result.entrySet()) {
+     
+             System.out.print(entry.getKey()+ " ");
+             System.out.println(" "+ entry.getValue());
+             System.out.println();
+          }  
+          in.close();
+       }
+       public static LinkedHashMap<String,String> sortMap(LinkedHashMap<String,String> map){
+            
+           List<Map.Entry<String,String>> list= new LinkedList<>(map.entrySet());
+        
+           Collections.sort(list, (o1,o2) -> o1.getValue().compareTo(o2.getValue()));
+
+           LinkedHashMap<String,String> lhm= new LinkedHashMap<>();
+           for(Map.Entry<String, String> entry: list) {
+               lhm.put(entry.getKey(), entry.getValue());
+           }
+           return lhm;
+       }
